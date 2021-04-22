40cc72ba2d26f674d77af4695d2e8c88fbc5a667
HeaDCraBee <theheadcrabz@gmail.com>
2021-04-22 17:46:30 +0300

    The module that stores the dictionary

---
+++ RomeArabicConverter/RomeToArabicDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+
+namespace RomeArabicConverter
+{
+    class RomeToArabicDictionary
+    {
+        static Dictionary<char, int> rTADictionary = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+        };
+
+        public static Dictionary<char, int> getRTADictionary()
+        {
+            return rTADictionary;
+        }
+    }
+
+}
