e2d42c325a0ac57888a3985be1c313c10be05e9f
HeaDCraBee <theheadcrabz@gmail.com>
2021-07-10 15:01:08 +0300

    master, ref (Program) change code to follow code conventions

--- RomeArabicConverter/Program.cs
+++ RomeArabicConverter/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RomeArabicConverter
 {
     class Program
     {
-       
-        
         static void Main(string[] args)
         {
-           
+
             Console.WriteLine("Which number system do you want to convert the number to?\n" +
                 " 1: from Arabic to Rome\n" +
                 " 2: from Rome to Arabic");
