117359276dc2e31b053fcfe07611945f5f7059f0
HeaDCraBee <theheadcrabz@gmail.com>
2021-07-10 15:00:55 +0300

    master, ref (Converter) change code to follow code conventions

--- RomeArabicConverter/Converter.cs
+++ RomeArabicConverter/Converter.cs
@@ -8,38 +8,33 @@ namespace RomeArabicConverter
 {
     public class Converter
     {
-        delegate double PowTen();
+        private delegate double PowTen();
+
         public static void Convert(string systemType, string number)
-        {
-            
-            if (systemType == "1")
-            {
-                Result = ToRome(int.Parse(number));
-            }
-            else if (systemType == "2")
-            {
-                Result = ToArabic(number.ToCharArray()).ToString();
-            }
+        {    
+            if (systemType == "1")            
+                Result = ToRome(int.Parse(number));            
+
+            else if (systemType == "2")           
+                Result = ToArabic(number.ToCharArray()).ToString();      
         }
 
         public static string Result { get; private set; }
-        
-        
 
         static int ToArabic(char[] number)
         {
-            var romeToArabicDictionary = RomeToArabicDictionary.getRTADictionary();
+            var romeToArabicDictionary = RomeToArabicDictionary.getRomeToArabicDictionary();
+
             foreach (var chars in number)
             {
-                if (!romeToArabicDictionary.ContainsKey(chars))
-                {
-                    throw new ArgumentException("Incorrect sentence");
-                }
+                if (!romeToArabicDictionary.ContainsKey(chars))               
+                    throw new ArgumentException("Incorrect sentence");            
             }
-            int res = romeToArabicDictionary[number[number.Length - 1]];
-            bool isLover = false;
+
+            int res = romeToArabicDictionary[number[number.Length - 1]];            
             int bigger = 0;
-            
+            bool isLover = false;
+
             /*
              * Начиная с конца строки начинает перевод из римских в арабские(справа на лево)
              * Если раньше встретилось число больше, то нынешнее число вычитается из текущего значения res.
@@ -48,6 +43,7 @@ namespace RomeArabicConverter
              * Так идя по числу CDXCIX(499) сначала посчитается последнее X(res = 10), из него вычтеться I (res = 9), 
              * к полученному результату прибавится С (res = 109), вычтится X (res = 99), прибавиться D (res = 599) и вычтиться C (res = 499)
              */
+
             for (int i = number.Length - 2; i > -1; i--)
             {
                 if (romeToArabicDictionary[number[i]] < romeToArabicDictionary[number[i + 1]] && !isLover)
@@ -56,16 +52,19 @@ namespace RomeArabicConverter
                     isLover = true;
                     bigger = romeToArabicDictionary[number[i + 1]];
                 }
+
                 else if (isLover && (romeToArabicDictionary[number[i]] < bigger))
                 {
                     throw new ArgumentException("Incorrect sentence");
                 }
+
                 else
                 {
                     res += romeToArabicDictionary[number[i]];
                     isLover = false;
                 }
             }
+
             return res;
         }
 
@@ -77,7 +76,8 @@ namespace RomeArabicConverter
         {
             Dictionary<int, char> arabicToRomeDictionary = new Dictionary<int, char>();
             int maxValueInRome = 0;
-            foreach (var pair in RomeToArabicDictionary.getRTADictionary())
+
+            foreach (var pair in RomeToArabicDictionary.getRomeToArabicDictionary())
             {
                 arabicToRomeDictionary.Add(pair.Value, pair.Key);
                 maxValueInRome = pair.Value;
@@ -85,12 +85,14 @@ namespace RomeArabicConverter
             
             int length = number.ToString().Length;
             StringBuilder romeStringBuilder = new StringBuilder();
+
             if (length >= maxValueInRome.ToString().Length)
             {
                 for (int i = 0; i < number / maxValueInRome; i++)
                 {
                     romeStringBuilder.Append(arabicToRomeDictionary[maxValueInRome]);
                 }
+
                 number %= maxValueInRome;
                 length = number.ToString().Length;
             }
@@ -104,6 +106,7 @@ namespace RomeArabicConverter
                 number %= (int)Math.Pow(10, length - 1);
                 length = number.ToString().Length;
             }
+
             return romeStringBuilder.ToString();
         }
 
@@ -116,27 +119,31 @@ namespace RomeArabicConverter
         static string AppendRomeStrings(int number, PowTen powTen, char smallerChar, char midleChar, char biggerChar)
         {
             StringBuilder stringBuilder = new StringBuilder();
+
             if (number / (int)powTen() < 4)
             {
-
                 for (int i = 0; i < number / (int)powTen(); i++)
                 {
                     stringBuilder.Append(smallerChar);
                 }
             }
+
             else if (number / (int)powTen() == 4)
             {
                 stringBuilder.Append(smallerChar);
                 stringBuilder.Append(midleChar);
             }
+
             else if (number / (int)powTen() < 9)
             {
                 stringBuilder.Append(midleChar);
+
                 for (int i = number / (int)powTen() - 5; i > 0; i--)
                 {
                     stringBuilder.Append(smallerChar);
                 }
             }
+
             else
             {
                 stringBuilder.Append(smallerChar);
